92eea15e40e421c8879078028612fed220834925
Gustavo Martínez de Murga <gustavo.murga1996@gmail.com>
2022-06-08 14:26:09 -0400

    Modifying the streaming file

--- src/streaming/streaming.controller.ts
+++ src/streaming/streaming.controller.ts
@@ -5,9 +5,39 @@ import { join } from 'path';
 @Controller('streaming')
 export class StreamingController {
 
-    @Get('base-general')
-    getFileCustomizedResponse(@Response({ passthrough: true }) res): StreamableFile {
-    const file = createReadStream(join(process.cwd(), 'bg.docx'));
+    @Get('base-general-internacional')
+    getFileBGInternacional(@Response({ passthrough: true }) res): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'bgInternacional.docx'));
+    res.set({
+      'Content-Type': 'application/json',
+      'Content-Disposition': 'attachment; filename="bg.docx',
+    })
+    return new StreamableFile(file);
+  }
+
+    @Get('base-general-nacional')
+    getFileBGNacional(@Response({ passthrough: true }) res): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'bgNacional.docx'));
+    res.set({
+      'Content-Type': 'application/json',
+      'Content-Disposition': 'attachment; filename="bg.docx',
+    })
+    return new StreamableFile(file);
+  }
+
+    @Get('base-general-excepcional')
+    getFileBGExcepcional(@Response({ passthrough: true }) res): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'bgExcepcional.docx'));
+    res.set({
+      'Content-Type': 'application/json',
+      'Content-Disposition': 'attachment; filename="bg.docx',
+    })
+    return new StreamableFile(file);
+  }
+
+    @Get('base-general-plaza')
+    getFileBGPlaza(@Response({ passthrough: true }) res): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'bgPlaza.docx'));
     res.set({
       'Content-Type': 'application/json',
       'Content-Disposition': 'attachment; filename="bg.docx',
