307dbf88f471333b738ebdfe18461652b0724092
daniel.acosta <david.daniel@seiza-ti.cl>
2022-06-15 13:30:26 -0400

    eliminado el cambio anterior

--- src/negociacion-resumen/negociacion-resumen.service.ts
+++ src/negociacion-resumen/negociacion-resumen.service.ts
@@ -180,12 +180,12 @@ export class NegociacionResumenService {
   async findAll(): Promise<NegociacionResumen[]> {
     return await this.negociacionResumenRepository.find({order: {
       fecha : "DESC",
-    }, relations: ['negociacionProveedores','fichaCompraResumen','negociacionProveedores.proveedor',
+    }, relations: ['negociacionProveedores','fichaCompraResumen',
     'solicitudContratacion','contratos']});
   }
 
   async findOne(id: number) : Promise<NegociacionResumen> {
-    return await this.negociacionResumenRepository.findOne(id,{ relations: ['negociacionProveedores','negociacionProveedores.proveedor',
+    return await this.negociacionResumenRepository.findOne(id,{ relations: ['negociacionProveedores',
     'fichaCompraResumen','solicitudContratacion','contratos']});
   }
 
