6254100b06a7efdddc71e8fdcee6b2868b807394
daniel acosta <david.daniel@seiza-ti.cl>
2022-08-23 13:31:59 -0400

    cambio al exception filter

--- src/all-exceptions.filter.ts
+++ src/all-exceptions.filter.ts
@@ -1,36 +1,38 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { Response } from "express";
 
-import {
-    ExceptionFilter,
-    Catch,
-    ArgumentsHost,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common';
-  import { HttpAdapterHost } from '@nestjs/core';
-  
-  @Catch()
-  export class AllExceptionsFilter implements ExceptionFilter {
-    constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
-  
-    catch(exception: unknown, host: ArgumentsHost): void {
-      // In certain situations `httpAdapter` might not be available in the
-      // constructor method, thus we should resolve it here.
-      const { httpAdapter } = this.httpAdapterHost;
-  
-      const ctx = host.switchToHttp();
-  
-      const httpStatus =
-        exception instanceof HttpException
-          ? exception.getStatus()
-          : HttpStatus.INTERNAL_SERVER_ERROR;
-  
-      const responseBody = {
-        statusCode: httpStatus,
-        timestamp: new Date().toISOString(),
-        path: httpAdapter.getRequestUrl(ctx.getRequest()),
-      };
-  
-      httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+@Catch(HttpException)
+export class AllExceptionsFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const context = host.switchToHttp();
+        const response = context.getResponse<Response>();
+        const request = context.getRequest<Request>();
+        const status = exception.getStatus();
+
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                timestamp: new Date().toISOString(),
+                path: request.url,
+            });
     }
-  }
-  
+}
+
+@Catch(!HttpException)
+export class unknow implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        const context = host.switchToHttp();
+        const response = context.getResponse<Response>();
+        const request = context.getRequest<Request>();
+        const status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                timestamp: new Date().toISOString(),
+                path: request.url,
+            });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  const httpAdapter  = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3001);
 }
 bootstrap();
