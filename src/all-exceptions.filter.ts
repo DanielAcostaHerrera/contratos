bbd00d853c4ebacda8d006cbda3798f1628537d5
daniel acosta <david.daniel@seiza-ti.cl>
2022-08-23 13:36:13 -0400

    segunda correccion al exception filter

--- src/all-exceptions.filter.ts
+++ src/all-exceptions.filter.ts
@@ -1,31 +1,15 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
 import { Response } from "express";
 
-@Catch(HttpException)
-export class AllExceptionsFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-        const context = host.switchToHttp();
-        const response = context.getResponse<Response>();
-        const request = context.getRequest<Request>();
-        const status = exception.getStatus();
-
-        response
-            .status(status)
-            .json({
-                statusCode: status,
-                timestamp: new Date().toISOString(),
-                path: request.url,
-            });
-    }
-}
-
-@Catch(!HttpException)
-export class unknow implements ExceptionFilter {
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: unknown, host: ArgumentsHost) {
         const context = host.switchToHttp();
         const response = context.getResponse<Response>();
         const request = context.getRequest<Request>();
-        const status = HttpStatus.INTERNAL_SERVER_ERROR;
+        const status = exception instanceof HttpException
+        ? (HttpException)(exception).getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
 
         response
             .status(status)
